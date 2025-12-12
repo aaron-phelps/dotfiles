835ba82d76ef80886a07b7d05901c1af9fb1594c
Aaron Phelps <aaron.c.phelps@gmail.com>
2025-12-11 18:30:12 -0600

    update theme for kitty and fish #10

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,12 @@
 if status is-interactive
     # Disable welcome message
     set -g fish_greeting
+
+    # White prompt colors
+    set -g fish_color_user white
+    set -g fish_color_host white
+    set -g fish_color_host_remote white
+    set -g fish_color_cwd white
 end
 
 if test "$TERM" = "xterm-kitty"
